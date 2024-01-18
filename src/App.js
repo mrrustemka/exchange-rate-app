7eead53064d611efc599db5497e55bba37261b33
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2024-01-18 07:47:25 +0400

    create-app-component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <input type="text" />
+      <select>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
     </div>
   );
 }
