4d309ba734c031daf86d3c1b62e3dc163157046a
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2024-01-18 19:55:58 +0400

    make-dynamic-request

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import "./App.css";
 
 function App() {
   const [value, setValue] = useState(0);
+  const [fromValue, setFromValue] = useState("USD");
+  const [toValue, setToValue] = useState("USD");
+
   useEffect(
     function () {
       async function fetchCurrency() {
         const result = fetch(
-          `https://api.frankfurter.app/latest?amount=${value}&from=GBP&to=USD`
+          `https://api.frankfurter.app/latest?amount=${value}&from=${fromValue}&to=${toValue}`
         )
           .then((resp) => resp.json())
           .then((data) => {
@@ -16,23 +19,31 @@ function App() {
       }
       fetchCurrency();
     },
-    [value]
+    [value, fromValue, toValue]
   );
 
   const handleChange = (event) => {
     setValue(event.target.value);
   };
 
+  const handleFromChange = (event) => {
+    setFromValue(event.target.value);
+  };
+
+  const handleToChange = (event) => {
+    setToValue(event.target.value);
+  };
+
   return (
     <div>
       <input type="text" onChange={handleChange} value={value} />
-      <select>
+      <select onChange={handleFromChange} value={fromValue}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select>
+      <select onChange={handleToChange} value={toValue}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
