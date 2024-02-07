cfe5f56c75eabd6732f6d436344af60a5e11ef94
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2024-02-07 19:50:46 +0400

    update-cirrencies-list

--- src/components/Currency.jsx
+++ src/components/Currency.jsx
@@ -11,8 +11,20 @@ function Currency({ value, setValue, isLoading }) {
       >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
+        <option value="JPY">JPY</option>
+        <option value="GBP">GBP</option>
+        <option value="CNY">CNY</option>
+        <option value="AUD">AUD</option>
         <option value="CAD">CAD</option>
+        <option value="CHF">CHF</option>
+        <option value="HKD">HKD</option>
+        <option value="SGD">SGD</option>
+        <option value="SEK">SEK</option>
+        <option value="KRW">KRW</option>
+        <option value="NOK">NOK</option>
+        <option value="NZD">NZD</option>
         <option value="INR">INR</option>
+        <option value="GEL">GEL</option>
       </select>
     </div>
   );
